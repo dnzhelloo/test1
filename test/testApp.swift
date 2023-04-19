7df70ebea00ad1a4019733a64d26b5b83f06d87b
dnzhelloo <dnzhelloo@icloud.com>
2023-04-19 11:12:08 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by deniz on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
